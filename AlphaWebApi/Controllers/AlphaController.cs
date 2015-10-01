574aa7cd9f61f8bd26b4e7164a4231810ee098c3
DaneVinson <dane.vinson@gmail.com>
2015-10-01 10:35:28 -0700

    Added solution to repo

---
+++ AlphaWebApi/Controllers/AlphaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AlphaWebApi.Controllers
+{
+    public class AlphaController : ApiController
+    {
+        private static readonly Version ControllerVersion = new Version(1, 0, 0, 0);
+
+        public string GetMessage()
+        {
+            return $"{this.GetType().Name} version {ControllerVersion}";
+        }
+    }
+}
---
+++ BravoWebApi/Controllers/BravoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BravoWebApi.Controllers
+{
+    public class BravoController : ApiController
+    {
+        private static readonly Version ControllerVersion = new Version(1, 0, 0, 0);
+
+        public string GetMessage()
+        {
+            return $"{this.GetType().Name} version {ControllerVersion}";
+        }
+    }
+}
